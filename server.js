ae126718e2688bf1269c8f0ad1798cd898c06a47
chilejay7 <codyburk7@yahoo.com>
2023-11-20 16:06:36 -0700

    adds notes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const session = require('express-session');
+
+// The routes exported from the controllers directory's main index.js file are imported.
 const routes = require('./controllers');
+
+// Imports the handlebars templating engine.
 const exphbs = require('express-handlebars');
 
+// Imports the connection configuration for the database from the file specified.
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -16,6 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+// Syncs the Sequelize models with the database.  The false option does not drop tables if they exist.
+// After the database is synced, the express application is started and set to listen on the given port.
 sequelize.sync({ force: false })
     .then(() => {
         app.listen(PORT, () => console.log(`Listening on ${PORT}`));
