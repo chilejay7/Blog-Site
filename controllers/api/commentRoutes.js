6ae6ba5d03e8e63da6b83c60e443c221342073af
chilejay7 <codyburk7@yahoo.com>
2023-11-24 11:53:21 -0700

    adds home route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     console.log(req);
+    const commentData = Comment.findAll();
     res.send('Comment request received.');
 });
 
