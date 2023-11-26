dfa2dcb7c44824cebe60ec35833119d45dc84c2a
chilejay7 <codyburk7@yahoo.com>
2023-11-26 15:49:53 -0700

    get post by id route added

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', async (req, res) => {
     // console.log(req);
@@ -14,5 +14,41 @@ router.get('/', async (req, res) => {
         loggedIn: req.session.loggedIn,
     });
 });
+
+router.get('/:id', async (req, res) => {
+    console.info(`The request is: ${req.params}`);
+    const { id } = req.params.id;
+    console.log(`The id retrieved from the request is: ${id}`);
+    const postId = await Post.findByPk(req.params.id, {
+       include: [
+        {
+            model: User,
+            attributes: [
+                'id',
+                'user_name',
+            ],
+        },
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                'comment_date',
+                'comment_content',
+                'user_id',
+                'post_id',
+            ],
+        },
+       ],
+    });
+
+    // const postEdit = postId.map(post => post.get({ plain: true }));
+    const postEdit = postId.get({ plain: true });
+    console.log(`The post retrieved from the database query is: ${postEdit.post_content}`);
+
+    res.render('updatePost', {
+        postEdit,
+        loggedIn: req.session.loggedIn,
+    })
+})
 
 module.exports = router;
