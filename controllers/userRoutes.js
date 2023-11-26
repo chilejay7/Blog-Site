363216b7308c13dd962a0dc0dc63ea49bcc736a2
chilejay7 <codyburk7@yahoo.com>
2023-11-26 12:58:10 -0700

    logout option added

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', (req, res) => {
-   res.render('user');
+    res.render('user');
 });
 
 // Login route
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
     // This will query the database using the name attribute of user_name set in the user.handlebars login form.
     const userData = await User.findOne({
         where: {
-           user_name: req.body.user_name,
+            user_name: req.body.user_name,
         }
     });
 
@@ -21,16 +21,16 @@ router.post('/', async (req, res) => {
     }
 
     // The checkPassword function is defined in the User model.  When the User class extends the Model parent, the new function is added to the class.
-     const validPassword = await userData.checkPassword(req.body.password);
-     console.log(`ValidPassword Data follows: ${validPassword}`);
+    const validPassword = await userData.checkPassword(req.body.password);
+    console.log(`ValidPassword Data follows: ${validPassword}`);
 
     // If the login password doesn't match the associated record in the database, a 400 status code is returned and stays on the login page.
-     if (!validPassword) {
+    if (!validPassword) {
         res
-          .status(400)
-          .render('user');
+            .status(400)
+            .render('user');
         return;
-      }
+    }
 
     // This creates a session variable with loggedIn values to save the user state.
     // Once set on a user's session, this is passed to the views through the controllers as a variable that can be used in conditional statements.
@@ -47,7 +47,7 @@ router.post('/', async (req, res) => {
 
 // Create new user route
 router.post('/create', async (req, res) => {
-    
+
     // The variables from the request are all set by the sign-up form's name attributes in the user.handlebars file.
     const userData = await User.create({
         user_name: req.body.user_name,
@@ -60,6 +60,11 @@ router.post('/create', async (req, res) => {
         res.status(200).redirect('/');
     })
     console.log(`Session data for debugging: ${req.session}`);
-})
+});
+
+router.post('/logout', async (req, res) => {
+    req.session.loggedIn ? req.session.destroy(() => res.status(204).end())
+        : res.status(404).end();
+});
 
 module.exports = router;
