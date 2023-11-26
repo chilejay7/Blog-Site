d508cfb2cc8b3f8030883c9b669f85e077a73247
chilejay7 <codyburk7@yahoo.com>
2023-11-25 17:37:23 -0700

    comments added to login routes

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,15 +2,14 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', (req, res) => {
-    // console.log(req.headers);
-    res.render('user');
-    // res.send('User request received');
+   res.render('user');
 });
 
 // Login route
 router.post('/', async (req, res) => {
     console.log(`Username is: ${req.body.user_name} and the password is: ${req.body.password}`);
 
+    // This will query the database using the name attribute of user_name set in the user.handlebars login form.
     const userData = await User.findOne({
         where: {
            user_name: req.body.user_name,
@@ -20,23 +19,26 @@ router.post('/', async (req, res) => {
     if (!userData) {
         res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
     }
-    // console.log(userData);
 
+    // The checkPassword function is defined in the User model.  When the User class extends the Model parent, the new function is added to the class.
      const validPassword = await userData.checkPassword(req.body.password);
      console.log(`ValidPassword Data follows: ${validPassword}`);
 
+    // If the login password doesn't match the associated record in the database, a 400 status code is returned and stays on the login page.
      if (!validPassword) {
         res
           .status(400)
-          .json({ message: 'Incorrect username or password. Please try again!' });
+          .render('user');
         return;
       }
 
-    // This creates a session variable to show that the user is logged in.  The user_id is used as the identifier with a boolean.
+    // This creates a session variable with loggedIn values to save the user state.
+    // Once set on a user's session, this is passed to the views through the controllers as a variable that can be used in conditional statements.
     req.session.save(async () => {
         req.session.user_id = userData.id;
         req.session.loggedIn = true;
 
+        // The redirect needs to be included with the response. Only one response can be sent to a single request.
         res.status(200).redirect('/');
     });
 
@@ -45,6 +47,8 @@ router.post('/', async (req, res) => {
 
 // Create new user route
 router.post('/create', async (req, res) => {
+    
+    // The variables from the request are all set by the sign-up form's name attributes in the user.handlebars file.
     const userData = await User.create({
         user_name: req.body.user_name,
         email: req.body.email,
@@ -53,10 +57,9 @@ router.post('/create', async (req, res) => {
 
     req.session.save(() => {
         req.session.loggedIn = true;
-        res.status(200);
+        res.status(200).redirect('/');
     })
     console.log(`Session data for debugging: ${req.session}`);
-    res.redirect('/');
 })
 
 module.exports = router;
