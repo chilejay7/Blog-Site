e7aae57a475512e067193a80e26bf69c4eb2dc54
chilejay7 <codyburk7@yahoo.com>
2023-11-26 15:10:33 -0700

    loggedIn value included in object sent to views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,11 @@ router.get('/', async (req, res) => {
     // console.log(postData);
     const posts = postData.map(post => post.get({ plain: true }));
     // console.log(posts);
-    res.render('homepage', { posts });
+    res.render('homepage', { 
+        posts,
+        // This value needs to be included in the object passed to each page in order to properly evaluate a user's session and login state.
+        loggedIn: req.session.loggedIn, 
+    });
 });
 
 module.exports = router;
