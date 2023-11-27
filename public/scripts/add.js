f41a4a9aec013e77dd897f3cf13bf1c033f006fc
chilejay7 <codyburk7@yahoo.com>
2023-11-27 07:22:19 -0700

    adds a text box to enter text for new post

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -1,7 +1,8 @@
 const addBtn = document.getElementById('addBtn');
 
-const addPost = () => {
+// Used to set the new location when the "Add a Post" button is clicked in the menu.
+const displayAdd = () => {
     document.location.replace('/api/posts/add');
-}
+};
 
-addBtn.addEventListener('click', addPost);
+addBtn.addEventListener('click', displayAdd);
