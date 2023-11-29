4cab94de9e7c55a8ea2905f83b31be66463b4bbe
chilejay7 <codyburk7@yahoo.com>
2023-11-29 16:10:19 -0700

    adds style and comments

--- public/scripts/post_delete.js
+++ public/scripts/post_delete.js
@@ -4,6 +4,9 @@ const deletePostBtn = document.getElementById('deletePostBtn');
 const deletePost = async (e) => {
     e.preventDefault();
     console.log(e);
+
+    // The window.location provides access to the page's URL, and the split method separates it based on the / symbols.
+    // This returns an array, which is why square brackets are used to then call the .length method of -1 to get the index of the id value we need.
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
