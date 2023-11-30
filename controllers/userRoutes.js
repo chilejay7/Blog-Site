fe8d0b8386b941c08a64301ec2b754969f9b1f85
chilejay7 <codyburk7@yahoo.com>
2023-11-29 23:14:52 -0700

    error handling added to login routes

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -7,21 +7,27 @@ router.get('/', (req, res) => {
 
 // Login route
 router.post('/', async (req, res) => {
-    console.log(`Username is: ${req.body.user_name} and the password is: ${req.body.password}`);
+
+    const { user_name, password } = req.body;
+    console.log(`Username is: ${user_name} and the password is: ${password}`);
 
     // This will query the database using the name attribute of user_name set in the user.handlebars login form.
     const userData = await User.findOne({
         where: {
-            user_name: req.body.user_name,
+            user_name: user_name.toLowerCase(),
         }
     });
 
     if (!userData) {
-        res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
+        res
+        .status(400)
+        .render('user')
+        // .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
     }
 
     // The checkPassword function is defined in the User model.  When the User class extends the Model parent, the new function is added to the class.
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await userData.checkPassword(password);
     console.log(`ValidPassword Data follows: ${validPassword}`);
 
     // If the login password doesn't match the associated record in the database, a 400 status code is returned and stays on the login page.
@@ -45,27 +51,33 @@ router.post('/', async (req, res) => {
 
 // Create new user route
 router.post('/create', async (req, res) => {
+    
+    const { user_name, email, password } = req.body;
 
-    // The variables from the request are all set by the sign-up form's name attributes in the user.handlebars file.
-    const userData = await User.create({
-        user_name: req.body.user_name,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    try {
+        // The variables from the request are all set by the sign-up form's name attributes in the user.handlebars file.
+        const userData = await User.create({
+            user_name: user_name.toLowerCase(),
+            email: email.toLowerCase(),
+            password,
+        });
 
-    const newUser = await User.findOne({
-        where: {
-            user_name: req.body.user_name,
-        }
-    });
+        const newUser = await User.findOne({
+            where: {
+                user_name,
+            }
+        });
 
 
-    req.session.save(() => {
-        req.session.user_id = newUser.id;
-        req.session.loggedIn = true;
-        res.status(200).redirect('/');
-    })
-    console.log(`Session data for debugging: ${req.session}`);
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.loggedIn = true;
+            res.status(200).redirect('/');
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 router.post('/logout', async (req, res) => {
